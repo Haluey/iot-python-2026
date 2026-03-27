572aeabdefa0b1642bc9f0557eaad6b4becb0ad2
Haluey <cheong7394wol@gmail.com>
2026-03-27 17:23:42 +0900

    1일차 소스링크 수정

--- day02/ex09_csv_read.py
+++ day02/ex09_csv_read.py
@@ -3,6 +3,6 @@
 with open('./day02/부산시_해운대구_도서정보.csv', 'r', encoding = 'utf-8') as f:
     # line = f.read()
     for line in f:
-        print(line.strip()) # strip?, '\n' 줄바꿈 제거
+        print(line.strip()) # '\n' 줄바꿈 제거
 
     print(line)
